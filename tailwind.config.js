5939c574445e69288172edbe034a5a65cfd11b2f
ej8899 <ej8899@gmail.com>
2024-03-01 17:28:35 -0500

    update: for tailwind reconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js}", "node_modules/preline/dist/*.js", 'node_modules/flowbite-react/lib/esm/**/*.js'],
+  
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    import("preline/plugin"), 
+    import("flowbite/plugin"),
+    import('flowbite/plugin')({
+      charts: true,
+    }),
+  ],
+
+  darkMode: 'class',
 }
+
 
