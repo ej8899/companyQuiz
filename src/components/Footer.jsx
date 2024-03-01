30ce07fb75e74b4fe9bd634634209bb567cd44bf
ej8899 <ej8899@gmail.com>
2024-03-01 15:07:12 -0500

    update: add styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,23 @@
 
 import { Footer } from 'flowbite-react';
 import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+import { globalconfig } from '../config.js';
 
 export default function MyFooter() {
   return (
-    
-      <div className="w-full text-slate-700 mt-4">
-        footer to build
-      </div>
+    <div className="bg-gray-800 text-white p-0 flex flex-row items-center justify-between px-4 bg-opacity-85 text-5xl font-thin fixed bottom-0 w-full -mt-6 border-0">
+    <div className="flex items-start items-center border-0">
+      
+      <span className="ml-2">{globalconfig.companyName}</span>
+    </div>
+    <div className="flex items-end items-center p-0 ml-4 border-0">
+    more here
+
+    </div>
+  </div>
   );
 }
+
+
+
+
