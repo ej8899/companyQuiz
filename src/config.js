69f9b051af09fb9a27d6a56cadf833e1210d7bd5
ej8899 <ej8899@gmail.com>
2024-03-01 08:26:33 -0500

    add: global config file

---
+++ src/config.js
@@ -0,0 +1,33 @@
+// config.js
+const config = {
+  //
+  // CONFIG for company deploying CompanyQuiz! app
+  //
+  companyName: "EJ Devs",
+  quizName: "Cyber Security Review",
+  passingGrade: 70,
+  numQuestions: 10,
+
+  
+  socialMediaLinks: {
+    website: "https://www.erniejohnson.ca",
+    twitter: "https://twitter.com/ejdevs",
+    facebook: "",
+    linkedin: "",
+    youtube: "",
+  },
+
+  // APP CONFIG
+
+
+
+  // DEVELOPER CONFIG:
+  link: {
+    github: "https://github.com/ej8899/",
+    linkedin: "https://www.linkedin.com/in/ernie-johnson/",
+    twitter: "http://www.twitter.com/ejdevscom",
+    youtube: "https://www.youtube.com/@oldmancodes",
+  },
+};
+
+export default config;
