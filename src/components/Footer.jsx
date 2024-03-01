583cf34a782fd41320704aa6a1c55f0b52fd8fbe
ej8899 <ej8899@gmail.com>
2024-03-01 11:48:21 -0500

    update: tweak footer layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+'use client';
+
+import { Footer } from 'flowbite-react';
+import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs';
+
+export default function MyFooter() {
+  return (
+    
+      <div className="w-full">
+        footer to build
+      </div>
+  );
+}
