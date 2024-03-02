0dba48f0e45b6e6dfab7e3557d284109c66f31d6
ej8899 <ej8899@gmail.com>
2024-03-02 14:41:31 -0500

    update: tweak style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function MyFooter() {
     <div className="bg-gray-800 text-white p-0 flex flex-row items-center justify-between px-4 bg-opacity-85 text-5xl font-thin fixed bottom-0 w-full -mt-6 border-0">
     <div className="flex items-start items-center border-0">
       
-      <span className="ml-2">{globalconfig.companyName}</span>
+      {/* <span className="ml-2">{globalconfig.companyName}</span> */}
     </div>
     <div className="flex items-end items-center p-0 ml-4 border-0">
     soc links here
