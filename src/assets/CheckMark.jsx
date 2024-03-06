9468e27ccc79f12aff28b08f9f3725d2ac661045
ej8899 <ej8899@gmail.com>
2024-03-06 06:19:49 -0500

    add: checkmark svg

---
+++ src/assets/CheckMark.jsx
@@ -0,0 +1,10 @@
+export function CheckMark() {
+  return (
+    <div>
+      <svg className="w-6 h-6 text-green-700 dark:text-green-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m5 12 4.7 4.5 9.3-9"/>
+      </svg>
+    </div>
+  );
+}
+
