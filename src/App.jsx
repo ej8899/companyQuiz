16af7e83d1de771ee0a3c5f443923c4a6399669d
ej8899 <ej8899@gmail.com>
2024-03-06 06:20:58 -0500

    update: tweak for testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import { Flowbite, DarkThemeToggle } from 'flowbite-react';
 import Navbar from './components/Navbar'
 import MyFooter from './components/Footer'
 import QuizApp from './pages/QuizApp'
+import Login from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import LandingPage from './pages/LandingPage';
 
 function App() {
 
   return (
     <Flowbite >
       <div className='h-screen'>
-      {/* <Navbar/> */}
-      <QuizApp/>
+      <Navbar/>
+      {/* <QuizApp/> */}
+      
+      <LandingPage />
+      <Login/>
+      <SignUp />
+
       <MyFooter />
       </div>
     </Flowbite>
