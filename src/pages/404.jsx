3c7f4679bdf3ed6d9261a3e55a22288489061ecf
ej8899 <ej8899@gmail.com>
2024-03-06 22:21:06 -0500

    add: 404 / not found page

---
+++ src/pages/404.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Button, } from 'flowbite-react';
+function NotFound() {
+
+  return (
+    <section className="bg-white dark:bg-gray-900 h-full items-center justify-center flex">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div className="mx-auto max-w-screen-sm text-center">
+                <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-gray-500 dark:text-primary-400 font-sans">Oops!</h1>
+                <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">That page doesn&apos;t exist.</p>
+                <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">You&apos;ll find lots to explore on the home page. </p>
+                <Button href="#" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300  rounded-lg text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</Button>
+            </div>   
+        </div>
+    </section>
+  );
+}
+
+export default NotFound;
