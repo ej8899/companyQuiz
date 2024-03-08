48aa21e87bb0208cee77bf6881bd0b54fd77aef3
ej8899 <ej8899@gmail.com>
2024-03-07 20:56:31 -0500

    add: admin page to build quizes

---
+++ src/pages/QuizAdmin.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Button, } from 'flowbite-react';
+
+function QuizAdmin() {
+
+  return (
+    <section className="bg-white dark:bg-gray-900 h-full items-center justify-center flex">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div className="mx-auto max-w-screen-sm text-center text-slate-400">
+                admin page for QUIZ abc
+            </div>   
+            <div>show individual quiz options (image, etc)</div>
+            <div>list the questions and possible ansers</div>
+        </div>
+    </section>
+  );
+}
+
+export default QuizAdmin;
