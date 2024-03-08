4bf9a208ed6d79b2ee338733c647c10dcd63ef43
ej8899 <ej8899@gmail.com>
2024-03-08 15:55:50 -0500

    fix: light dark mode issues

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -10,7 +10,7 @@ function NotFound() {
                 <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-gray-500 dark:text-primary-400 font-sans">Oops!</h1>
                 <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">That page doesn&apos;t exist.</p>
                 <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">You&apos;ll find lots to explore on the home page. </p>
-                <Button href="#" className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300  rounded-lg text-lg px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</Button>
+                <Button href="#" className="">Back to Homepage</Button>
             </div>   
         </div>
     </section>
