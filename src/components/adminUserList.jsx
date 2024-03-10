3ef07ed135ca49f78412978787bba99f0b67303c
ej8899 <ej8899@gmail.com>
2024-03-10 17:51:23 -0400

    update: tweak styles

--- src/components/adminUserList.jsx
+++ src/components/adminUserList.jsx
@@ -1,4 +1,4 @@
-import { Button,  } from 'flowbite-react';
+import { Button,  Tooltip} from 'flowbite-react';
 
 
 import { useState } from 'react';
@@ -55,22 +55,23 @@ export function AdminUserList() {
     # TODO - need add a user button
     # TODO - add quiz to individual User (displays list of available copmany quizes)
   
-    <table className="min-w-full divide-y divide-gray-200 w-full border-2 rounded-xl border-gray-200 shadow-md">
+    <div className="border-1 border border-separate rounded-xl border-gray-200 shadow-md overflow-hidden w-full">
+    <table className="min-w-full divide-y divide-gray-200 w-full table-auto">
       <thead className="bg-gray-50 border-0">
         <tr className="border-0 p-4">
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="text-right px-6 py-3 text-left text-s font-medium text-gray-500 uppercase tracking-wider">
             User ID
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="text-left px-6 py-3 text-left text-s font-medium text-gray-500 uppercase tracking-wider">
             Name
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="text-left px-6 py-3 text-left text-s font-medium text-gray-500 uppercase tracking-wider">
             Email
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="text-left px-6 py-3 text-left text-s font-medium text-gray-500 uppercase tracking-wider">
             Status
           </th>
-          <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+          <th scope="col" className="text-left px-6 py-3 text-left text-s font-medium text-gray-500 uppercase tracking-wider">
             Actions
           </th>
         </tr>
@@ -79,47 +80,58 @@ export function AdminUserList() {
       {sortedUserData.map((user) => (
           <>
             <tr key={user.userId} className="text-gray-500 border-0 p-8">
-              <td className="p-4">{user.userId}</td>
-              <td className="w-full">{user.name}</td>
-              <td>{user.email}</td>
-              <td>
-              <div className="flex h-4 mr-4">
-                <div className="bg-red-500" style={{ width: `${calculateScoreTotals(user.scores).nullScorePercentage}%` }}></div>
-                <div className="bg-yellow-500" style={{ width: `${calculateScoreTotals(user.scores).belowPassingGradePercentage}%` }}></div>
-                <div className="bg-green-500" style={{ width: `${calculateScoreTotals(user.scores).abovePassingGradePercentage}%` }}></div>
+              <td className="p-4 text-right">{user.userId}</td>
+              <td className="text-left">{user.name}</td>
+              <td className="text-left">{user.email}</td>
+              <td className="">
+              <div className="flex mr-4 border-0 border-gray-200 rounded-lg overflow-hidden">
+              
+                <div className="bg-red-500 pl-2" style={{ width: `${calculateScoreTotals(user.scores).nullScorePercentage}%` }}><Tooltip content="quizes not taken">{calculateScoreTotals(user.scores).nullScorePercentage}%</Tooltip></div>
+              
+                <div className="bg-yellow-500 pl-2" style={{ width: `${calculateScoreTotals(user.scores).belowPassingGradePercentage}%` }}><Tooltip content="quizes failed">{calculateScoreTotals(user.scores).belowPassingGradePercentage}%</Tooltip></div>
+              
+                <div className="bg-green-500 pl-2" style={{ width: `${calculateScoreTotals(user.scores).abovePassingGradePercentage}%` }}><Tooltip content="quizes passed">{calculateScoreTotals(user.scores).abovePassingGradePercentage}%</Tooltip></div>
               </div>
               </td>
-              <td>
+              <td className="text-left">
                 <div className="flex flex-row">
-                  <FaEdit className="mr-4 h-8 w-8"/>
-                  <BsFillTrashFill className="mr-4 h-8 w-8"/>
-                  <button onClick={() => toggleRow(user.userId)}>Show Scores</button>
+                  <Tooltip content="edit user details"><FaEdit className="mr-4 h-6 w-6"/></Tooltip>
+                  <Tooltip content="delete this user from company"><BsFillTrashFill className="mr-4 h-6 w-6"/></Tooltip>
+                  <button onClick={() => toggleRow(user.userId)}> {openRows[user.userId] ? "Hide Details" : "Show Details"}</button>
+                   
                 </div>
               </td>
             </tr>
             {openRows[user.userId] && (
               <tr key={`${user.userId}-scores`} className="text-gray-500 p-8">
-                <td colSpan="4">
-                  <table className="w-full">
-                    <thead>
-                      <tr>
-                        <th>Quiz ID</th>
-                        <th>Score</th>
-                        <th>Date Tested</th>
-                        <th>Actions +addQuiz</th>
-                      </tr>
+                <td colSpan="5" className="w-full justify-end border-0 text-right">
+                  <div className="border-1 border border-separate rounded-xl border-gray-300 shadow-md overflow-hidden w-full justify-end flex flex-col">
+                  <table className="w-full shadow-md m-0 p-0 table-auto">
+                    <thead className="p-0 m-0 bg-gray-300 border-2 border-gray-200 rounded-xl border">
+                      {/* <tr className="bg-gray-300 border-2 border-gray-200 rounded-xl border"> */}
+                        <th className="text-s font-medium text-right uppercase tracking-wider">Quiz ID</th>
+                        <th className="text-s font-medium text-center uppercase tracking-wider">Score</th>
+                        <th className="text-s font-medium uppercase tracking-wider">Date Tested</th>
+                        <th className="text-s font-medium text-left uppercase tracking-wider">Actions +addQuiz</th>
+                      {/* </tr> */}
                     </thead>
                     <tbody>
                       {user.scores.map((score, index) => (
                         <tr key={index}>
-                          <td>{score.quizId}</td>
-                          <td>{score.score}</td>
+                          <td className="pr-4">{score.quizId}</td>
+                          <td className="text-center">
+                            <div className={`text-center rounded-full border-1 border-white p-0 m-2 ${isNaN(score.score) || !score.score ? 'bg-red-500' : score.score < 70 ? 'bg-yellow-500' : 'bg-green-500'}`}>
+                            {score.score !== null ? score.score : '--'}
+                            </div>
+                          </td>
+
                           <td>{score.dateTested}</td>
-                          <td><RxReset /> removeQuiz</td>
+                          <td><div className="flex flex-row p-2"><Tooltip content="reset quiz to not started"><RxReset className="w-6 h-6 ml-2 mr-2"/></Tooltip> <Tooltip content="delete quiz from user"><BsFillTrashFill  className="w-6 h-6"/></Tooltip></div></td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
+                  </div>
                 </td>
               </tr>
             )}
@@ -127,7 +139,7 @@ export function AdminUserList() {
         ))}
       </tbody>
     </table>
-
+    </div>
   </section>
   </>
   );
