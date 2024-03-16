e3be25229e9a4ccdb4ad4312130df13cef51e70a
ej8899 <ej8899@gmail.com>
2024-03-16 15:09:21 -0400

    update: tweak cookie banner info

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -1,10 +1,14 @@
-import { Button, Banner } from 'flowbite-react';
+import { Button, Banner, Modal } from 'flowbite-react';
 import { PiCookieFill } from "react-icons/pi";
 import { BsCookie } from "react-icons/bs";
 import { BsXLg } from "react-icons/bs";
 
+import { useState } from 'react';
+
 export function CookieBanner() {
+  const [openModal, setOpenModal] = useState(false);
   return (
+    <>
     <Banner>
     <div className="z-50 flex fixed bottom-0 left-0 w-full flex-row justify-between border-t-2 border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
       <div className="mx-auto flex items-center">
@@ -13,18 +17,41 @@ export function CookieBanner() {
 
           <PiCookieFill className="h-12 w-12 mr-4"/>
           <span className="[&_p]:inline">
-          We use our own cookies as well as third-party cookies on our websites to enhance your experience, analyze our traffic, and for security and marketing. Select "Accept All" to allow them to be used. Read our Cookie Policy.
+          This site uses cookies for performance, analytics, and personalization which helps us improve our site. This will be set only after consent. For more details relative to cookies and other sensitive data, please read the full privacy policy.
           </span>
         </p>
       </div>
       <div className="flex flex-shrink-0 space-x-4 justify-center items-center ml-8 mr-8">
-        <a href="">Manage Settings</a>
+        {/* <Button onClick={() => setOpenModal(true)}>Manage Settings</Button> */}
         <Banner.CollapseButton>Accept All</Banner.CollapseButton>
-        <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
+        {/* <Banner.CollapseButton color="gray" className="border-0 bg-transparent text-gray-500 dark:text-gray-400">
           <BsXLg className="h-4 w-4"/>
-        </Banner.CollapseButton>
+        </Banner.CollapseButton> */}
       </div>
     </div>
     </Banner>
+
+    <Modal show={openModal} onClose={() => setOpenModal(false)}>
+      <Modal.Header>Cookies Management</Modal.Header>
+      <Modal.Body>
+        <div className="space-y-6">
+          <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Currently, we&apos;re only utilizing cookies that are required for the functionality of this website. No cookies are being used for tracking and advertising purposes. If such cookies are utilized in the future, you will be notified and allow to turn them off here.
+          </p>
+          {/* <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
+            to ensure a common set of data rights in the European Union. It requires organizations to notify users as
+            soon as possible of high-risk data breaches that could personally affect them.
+          </p> */}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => setOpenModal(false)}>Continue</Button>
+        {/* <Button color="gray" onClick={() => setOpenModal(false)}>
+          Decline
+        </Button> */}
+      </Modal.Footer>
+      </Modal>
+</>
   );
 }
