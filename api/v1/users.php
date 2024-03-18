e588759e536ccbf2bca8bba5196714dacbe80055
ej8899 <ej8899@gmail.com>
2024-03-17 21:20:42 -0400

    fix: cors issues fixed

--- api/v1/users.php
+++ api/v1/users.php
@@ -210,6 +210,23 @@ function addUser($data) {
 // Handle GET requests
 //
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+  // Allow from any origin
+  if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  }
+
+  // Handle preflight OPTIONS request
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+          header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+          header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+      exit(0);
+  }
+
   // If the request method is GET
   if (isset($_GET['uid']) && !empty($_GET['uid'])) {
       // If UID is specified in the URL, retrieve the user by UID
