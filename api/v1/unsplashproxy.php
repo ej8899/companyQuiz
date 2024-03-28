af8794b02e86059d171cc33bed91620e7cef97eb
ej8899 <ej8899@gmail.com>
2024-03-28 08:54:57 -0400

    fix: multiple keyword bug

--- api/v1/unsplashproxy.php
+++ api/v1/unsplashproxy.php
@@ -2,11 +2,11 @@
   require('config.php');
 
   header("Access-Control-Allow-Origin: *");
-  $searchTerm = $_GET['query'];
+  $searchTerm = urlencode($_GET['query']);
 
   // Set the Unsplash API endpoint and access key
-  $unsplashEndpoint = "https://api.unsplash.com/search/photos?query={$searchTerm}&orientation=landscape&client_id={$unsplashkey}";
-
+  $unsplashEndpoint = "https://api.unsplash.com/search/photos?query={$searchTerm}&w=200&count=20&orientation=landscape&client_id={$unsplashkey}";
+  
   // Make the request to Unsplash API
   $response = file_get_contents($unsplashEndpoint);
 
